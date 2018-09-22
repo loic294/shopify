f69da8f5f326f15ae45fd95dc5c45d199422b635
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-09-22 14:15:51 -0400

    Fixed fetch

--- src/queries/index.js
+++ src/queries/index.js
@@ -2,9 +2,11 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import fetch from '@brillout/fetch'
 
 const httpLink = createHttpLink({
   uri: 'https://api.github.com/graphql',
+  fetch: fetch
 });
 
 const authLink = setContext((_, { headers }) => {
