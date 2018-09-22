8a65650c59b166cb628f07b24adcabbb37407745
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-09-22 16:42:21 -0400

    Favicon fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'My Github Favorites',
+        short_name: 'Favorites',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#7433F1',
+        theme_color: '#7433F1',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
